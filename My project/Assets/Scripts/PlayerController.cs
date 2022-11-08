4435543644c8208a7815911085a23791403ee810
SkeletPaw <80264773+SkeletPaw@users.noreply.github.com>
2022-11-09 03:06:19 +0600

    jj

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float playerSpeed;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * playerSpeed);
     }
 }
